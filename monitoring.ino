d0ba1339e3619f0260f2c6c8bd0a65e6d1d6dd91
Ole August <oleastole@gmail.com>
2022-05-06 15:26:56 +0200

    feat: retrieve ESP-chip-information

--- monitoring.ino
+++ monitoring.ino
@@ -1,24 +1,25 @@
-#include <DHT.h>
+#include <string>
 #include "WiFi.h"
+#include <DHT.h>
 #include <PubSubClient.h>
 
 // --- CONSTANTS ---
 #define PIN_DHT 12
 #define DEFAULT_SENSE_FREQUENCY 2000
 
-const char* SSID = "GUEST-FASTWEB-B37487";
-const char* PASS = "zPxg9ax5nV";
+const char *SSID = "GUEST-FASTWEB-B37487";
+const char *PASS = "zPxg9ax5nV";
 
-const char* IP_MQTT_SERVER="130.136.2.70";
-const char* MQTT_USER = "iot2020";
-const char* MQTT_PASSWD = "mqtt2020*";
+const char *IP_MQTT_SERVER = "130.136.2.70";
+const char *MQTT_USER = "iot2020";
+const char *MQTT_PASSWD = "mqtt2020*";
 
-const char* TOPIC_0="sensor/temp";
-const char* TOPIC_1="sensor/hum";
+const char *TOPIC_0 = "sensor/temp";
+const char *TOPIC_1 = "sensor/hum";
 
 // --- VARIABLES ---
 
-PubSubClient clientMQTT; 
+PubSubClient clientMQTT;
 WiFiClient clientWiFi;
 
 DHT dht(PIN_DHT, DHT22);
@@ -34,20 +35,40 @@ struct SensorData {
   float hum;
 };
 
+struct BoardData {
+  uint32_t chipId;
+  uint8_t cores;
+  uint8_t chipRevision;
+  std::string chipModel;
+};
+
 // --- FUNCTIONS ---
 
-void setup() {
-  Serial.begin(115200);
-  delay(100);
-  dht.begin();
-  delay(2000);
-  connect_wifi();
-  setup_mqtt();
+struct BoardData chip_info() {
+  struct BoardData board_data;
+  
+  uint32_t chipId = 0;
+  for (int i = 0; i < 17; i = i + 8) {
+    chipId |= ((ESP.getEfuseMac() >> (40 - i)) & 0xff) << i;
+  }
+
+  board_data.chipId = chipId;
+  board_data.cores = ESP.getChipCores();
+  board_data.chipModel = ESP.getChipModel();
+  board_data.chipRevision = ESP.getChipRevision();
+
+  Serial.printf("ESP32 Chip model = %s Rev %d\n", board_data.chipModel,
+                board_data.chipRevision);
+  Serial.printf("This chip has %d cores\n", board_data.cores);
+  Serial.print("Chip ID: ");
+  Serial.println(board_data.chipId);
+
+  return board_data;
 }
 
 void setup_mqtt() {
   clientMQTT.setClient(clientWiFi);
-  clientMQTT.setServer(IP_MQTT_SERVER,1883);
+  clientMQTT.setServer(IP_MQTT_SERVER, 1883);
   clientMQTT.setBufferSize(400);
   resultMQTT = false;
 }
@@ -66,19 +87,20 @@ void connect_wifi() {
 boolean publishData(int channel, float value) {
   bool result = false;
   boolean connected = clientMQTT.connected();
-  
-  if (!connected) 
+
+  if (!connected)
     connected = clientMQTT.connect("airQualitySensor", MQTT_USER, MQTT_PASSWD);
   if (connected) {
-      String message = String(value);
-      const char* payload = message.c_str();
-      if (channel == 0) 
-        result = clientMQTT.publish(TOPIC_0, payload);
-      else
-       result = clientMQTT.publish(TOPIC_1, payload);
-      clientMQTT.loop();
-      return result;
-  } else return(false);  
+    String message = String(value);
+    const char *payload = message.c_str();
+    if (channel == 0)
+      result = clientMQTT.publish(TOPIC_0, payload);
+    else
+      result = clientMQTT.publish(TOPIC_1, payload);
+    clientMQTT.loop();
+    return result;
+  } else
+    return (false);
 }
 
 struct SensorData read_sensor_data() {
@@ -96,17 +118,27 @@ void log_sensor_data(struct SensorData sensor_data) {
   Serial.print(sensor_data.hum);
 }
 
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+  dht.begin();
+  delay(2000);
+  connect_wifi();
+  setup_mqtt();
+  chip_info();
+}
+
 void loop() {
   struct SensorData sensor_data = read_sensor_data();
   log_sensor_data(sensor_data);
 
-  resultMQTT = publishData(0,tempValue);
+  resultMQTT = publishData(0, tempValue);
   resultMQTT = publishData(1, humValue);
 
-  if (resultMQTT) 
+  if (resultMQTT)
     Serial.println("[LOG] Data temp published on the MQTT server");
   else
     Serial.println("[ERROR] MQTT connection failed");
-  
+
   delay(DEFAULT_SENSE_FREQUENCY);
 }
